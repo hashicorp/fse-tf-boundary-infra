66efe62b204e32febaf74b654a2f580e56525888
Luke McCleary <luke.mccleary@gmail.com>
2021-12-15 14:05:32 -0500

    opened port for fault

--- sg.tf
+++ sg.tf
@@ -20,6 +20,15 @@ resource "aws_security_group_rule" "allow_9200_controller" {
   security_group_id = aws_security_group.controller.id
 }
 
+resource "aws_security_group_rule" "allow_8200_controller" {
+  type              = "ingress"
+  from_port         = 8200
+  to_port           = 8200
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.controller.id
+}
+
 resource "aws_security_group_rule" "allow_9201_controller" {
   type              = "ingress"
   from_port         = 9201

d679bf372eb84b9304fe0d287f206560abdda3c8
Luke McCleary <luke.mccleary@gmail.com>
2021-12-16 11:04:31 -0500

    removed jq

--- ec2.tf
+++ ec2.tf
@@ -120,7 +120,6 @@ resource "aws_instance" "vault" {
       "mkdir ~/vault",
       "mv ~/config.hcl ~/vault/config.hcl",
       #setup docker
-      "sudo apt-get -y install jq",
       "sudo apt-get install ca-certificates curl gnupg lsb-release",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
       "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",

2644212dd172b9b15da7a75fa459c2cebd2b35dd
Luke McCleary <luke.mccleary@gmail.com>
2021-12-20 17:39:33 -0500

    changing sg format

--- sg.tf
+++ sg.tf
@@ -1,182 +1,139 @@
 resource "aws_security_group" "controller" {
   name = "controller_sg"
   vpc_id = aws_vpc.main.id
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 9200
+        to_port = 9200
+        protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+      # allow everyone access to psql. Do not do in production
+        from_port = 5432
+        to_port = 5432
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+      # inbound from private subnet
+      from_port         = 0
+      to_port           = 0
+      protocol          = -1
+      cidr_blocks       = [aws_subnet.private.cidr_block]
+    }
 }
 
 resource "aws_security_group" "worker" {
   name = "worker_sg"
   vpc_id = aws_vpc.main.id
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 9202
+        to_port = 9202
+        protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 8000
+        to_port = 8000
+        protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+      from_port         = 0
+      to_port           = 0
+      protocol          = -1
+      cidr_blocks       = [aws_subnet.private.cidr_block]
+    }
 }
 
 resource "aws_security_group" "vault" {
   name = "vault_sg"
   vpc_id = aws_vpc.main.id
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+      ingress {
+        # should be using bastion for vault
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+     ingress {
+       # allowing gloabl access for UI demo and build. Do not do this in production
+       from_port   = 8200
+        to_port      = 8205
+        protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+      from_port         = 0
+      to_port           = 0
+      protocol          = -1
+      cidr_blocks       = [aws_subnet.private.cidr_block]
+    }
 }
 
 resource "aws_security_group" "tfc_agent" {
   name = "tfc_Agent_sg"
   vpc_id = aws_vpc.main.id
+      egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+      # inbound bastion access for provisioning. Can be disabled after provisoining.
+        from_port = 22
+        to_port = 22
+        protocol = "ssh"
+        cidr_blocks = ["${aws_instance.controller.private_ip}/32"]
+    }
+    
 }
 
-resource "aws_security_group_rule" "allow_ssh_controller" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.controller.id
-}
-
-# Allow worker to talk to tfc_agent for provisioning. Not needed in production
-resource "aws_security_group_rule" "allow_ssh_tfc" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["${aws_instance.controller.private_ip}/32"]
-  security_group_id = aws_security_group.tfc_agent.id
-}
-
-resource "aws_security_group_rule" "allow_ssh_worker" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.worker.id
-}
-resource "aws_security_group_rule" "allow_ssh_vault" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.vault.id
-}
-
-resource "aws_security_group_rule" "allow_8200_vault" {
-  type              = "ingress"
-  from_port         = 8200
-  to_port           = 8205
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.vault.id
-}
-
-resource "aws_security_group_rule" "allow_egress_vault" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.vault.id
-}
-resource "aws_security_group_rule" "allow_9200_controller" {
-  type              = "ingress"
-  from_port         = 9200
-  to_port           = 9200
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.controller.id
-}
+#resource "aws_security_group_rule" "allow_9201_controller" {
+#  type              = "ingress"
+#  from_port         = 9201
+#  to_port           = 9201
+#  protocol          = "tcp"
+#  cidr_blocks       = ["0.0.0.0/0"]
+#  security_group_id = aws_security_group.controller.id
+#}
 
 
-resource "aws_security_group_rule" "allow_9201_controller" {
-  type              = "ingress"
-  from_port         = 9201
-  to_port           = 9201
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.controller.id
-}
-
-resource "aws_security_group_rule" "allow_443_controller" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.controller.id
-}
-
-resource "aws_security_group_rule" "allow_5432_controller" {
-  type              = "ingress"
-  from_port         = 5432
-  to_port           = 5432
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.controller.id
-}
-
-resource "aws_security_group_rule" "allow_egress_controller" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.controller.id
-}
-
-resource "aws_security_group_rule" "allow_web_worker" {
-  type              = "ingress"
-  from_port         = 8000
-  to_port           = 8000
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.worker.id
-}
-
-resource "aws_security_group_rule" "allow_9202_worker" {
-  type              = "ingress"
-  from_port         = 9202
-  to_port           = 9202
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.worker.id
-}
-
-resource "aws_security_group_rule" "allow_egress_worker" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.worker.id
-}
-
-# TFC_agent Rules
-resource "aws_security_group_rule" "allow_outbound_tfc" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.tfc_agent.id
-}
-
-resource "aws_security_group_rule" "allow_private_to_controller" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
-  cidr_blocks       = [aws_subnet.private.cidr_block]
-  security_group_id = aws_security_group.controller.id
-}
-
-resource "aws_security_group_rule" "allow_private_to_worker" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
-  cidr_blocks       = [aws_subnet.private.cidr_block]
-  security_group_id = aws_security_group.worker.id
-}
-
-resource "aws_security_group_rule" "allow_private_to_vault" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
-  cidr_blocks       = [aws_subnet.private.cidr_block]
-  security_group_id = aws_security_group.vault.id
-}

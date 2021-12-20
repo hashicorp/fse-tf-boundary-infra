72bbf138e5f9d11fd7c5640f3c558beb48c2d474
Luke McCleary <luke.mccleary@gmail.com>
2021-12-20 17:46:37 -0500

    labeling rules

--- sg.tf
+++ sg.tf
@@ -2,18 +2,21 @@ resource "aws_security_group" "controller" {
   name   = "controller_sg"
   vpc_id = aws_vpc.main.id
   egress {
+    # allow all outbound
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    # Allow inbound SSS from anywhere
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    # Allow access to Boundary UI from anywhere
     from_port   = 9200
     to_port     = 9200
     protocol    = "tcp"
@@ -27,13 +30,14 @@ resource "aws_security_group" "controller" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    # Allow access to Boundary https from anywhere
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    # inbound from private subnet
+    # allow all inbound traffic from private subnet
     from_port   = 0
     to_port     = 0
     protocol    = -1
@@ -46,12 +50,14 @@ resource "aws_security_group" "worker" {
   vpc_id = aws_vpc.main.id
 
   egress {
+    # allow all outbound
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    # Allow inbound SSH from anywhere
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -64,12 +70,14 @@ resource "aws_security_group" "worker" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    # inbound webapp access for worker
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    # allow all inbound traffic from private subnet
     from_port   = 0
     to_port     = 0
     protocol    = -1
@@ -81,12 +89,14 @@ resource "aws_security_group" "vault" {
   name   = "vault_sg"
   vpc_id = aws_vpc.main.id
   egress {
+    # allow all outbound
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    # Allow inbound SSS from anywhere
     # should be using bastion for vault
     from_port   = 22
     to_port     = 22
@@ -101,6 +111,7 @@ resource "aws_security_group" "vault" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    # allow all inbound traffic from private subnet
     from_port   = 0
     to_port     = 0
     protocol    = -1
@@ -112,6 +123,7 @@ resource "aws_security_group" "tfc_agent" {
   name   = "tfc_Agent_sg"
   vpc_id = aws_vpc.main.id
   egress {
+    # allow all outbound
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -121,7 +133,7 @@ resource "aws_security_group" "tfc_agent" {
     # inbound bastion access for provisioning. Can be disabled after provisoining.
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "tcp"
     cidr_blocks = ["${aws_instance.controller.private_ip}/32"]
   }
 
